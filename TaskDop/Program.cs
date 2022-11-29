8939554d288de685cc4037794d946c323193f1eb
Chub <ychubykin@gmail.com>
2022-11-29 11:11:17 +0300

    homework down

--- TaskDop/Program.cs
+++ TaskDop/Program.cs
@@ -3,9 +3,19 @@
 // последовательности, то есть элемента, который будет наибольшим, если 
 // из последовательности удалить наибольший элемент.
 
+Console.Clear();
+Console.WriteLine("Введите последовательность чисел с 0 в конце");
 int n = Convert.ToInt32(Console.ReadLine());
+int maxFirst = n, maxSecond = -1;
 while (n != 0)
 {
     n = Convert.ToInt32(Console.ReadLine());
+    if (n > maxFirst)
+    {
+        maxSecond = maxFirst;
+        maxFirst = n;
+    }
+    else if (n > maxSecond)
+        maxSecond = n;
 }
-Console.Write("end");
+Console.WriteLine(maxSecond);
